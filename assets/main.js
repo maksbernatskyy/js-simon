4e412c48bf5c3857213e9cf032e4a3eb3b493d59
maksbernatskyy <maksbernatskyy@gmail.com>
2025-10-16 16:44:43 +0200

    check the numbers correct

--- assets/main.js
+++ assets/main.js
@@ -29,39 +29,42 @@ const randomNumbersField = document.getElementById("random-numbers");
 // Contenitore dei numeri generati
 const randomNumbers = [];
 
-// Contenitore dei numeri dell'utente
-const numbers = []
+// Contenitore dei numeri corretti
+const checkNumbers = [];
+
+// Contatore dei numeri corretti
+let checkCounter = 0;
 
 // Collegamento al blocco da far scomparire
-const timerAndNumbersField = document.getElementById('timer-and-numbers')
+const timerAndNumbersField = document.getElementById("timer-and-numbers");
 
 // Collegamento alla lista di input nell'HTML
-const inputList = document.getElementById('list-input')
+const inputList = document.getElementById("list-input");
 
 // Totale di tempo per memorizzare
-let counterEl = 5
+let counterEl = 5;
 
 // Collegamento al timer dell'HTML
-const counterField = document.getElementById('timer')
-counterField.innerHTML = counterEl
+const counterField = document.getElementById("timer");
+counterField.innerHTML = counterEl;
 
 // Collegamento al form
-const form = document.getElementById('form')
+const form = document.getElementById("form");
 
 // Primo numero dell'utente
-const numbers1Field = document.getElementById('number1')
+const numbers1Field = document.getElementById("number1");
 
 // Secondo numero dell'utente
-const numbers2Field = document.getElementById('number2')
+const numbers2Field = document.getElementById("number2");
 
 // Terzo numero dell'utente
-const numbers3Field = document.getElementById('number3')
+const numbers3Field = document.getElementById("number3");
 
 // Quarto numero dell'utente
-const numbers4Field = document.getElementById('number4')
+const numbers4Field = document.getElementById("number4");
 
 // Quinto numero dell'utente
-const numbers5Field = document.getElementById('number5')
+const numbers5Field = document.getElementById("number5");
 
 // ====================================================================
 // ====================================================================
@@ -84,20 +87,19 @@ randomNumbersField.innerHTML = randomNumbers;
 
 // Avvio timer per il countdown
 const clock = setInterval(() => {
-    counterEl--
-    counterField.innerHTML = counterEl
+  counterEl--;
+  counterField.innerHTML = counterEl;
 
-    if (counterEl === 0) {
-        clearInterval(clock)
-    }
-
-}, 1000)
+  if (counterEl === 0) {
+    clearInterval(clock);
+  }
+}, 1000);
 
 // Avvio timer la sostituzione dai numeri al form
 setTimeout(() => {
-    timerAndNumbersField.classList.toggle('d-none')
-    inputList.classList.toggle('d-none')
-}, 5000)
+  timerAndNumbersField.classList.toggle("d-none");
+  inputList.classList.toggle("d-none");
+}, 5000);
 
 // ====================================================================
 // ====================================================================
@@ -106,20 +108,35 @@ setTimeout(() => {
 
 // Funzione per mantenere i dati del form
 form.addEventListener("submit", (event) => {
-    event.preventDefault()
+  event.preventDefault();
 
-    numbers.push(numbers1Field.value)
+  // Inserisco i numeri dell'utente in un contenitore
 
-    numbers.push(numbers2Field.value)
+  // Contenitore dei numeri dell'utente
+  const numbers = [
+    Number(numbers1Field.value),
+    Number(numbers2Field.value),
+    Number(numbers3Field.value),
+    Number(numbers4Field.value),
+    Number(numbers5Field.value),
+  ]
 
-    numbers.push(numbers3Field.value)
+  for (let i = 0; i < numbers.length; i++) {
+    const thisNumber = numbers[i];
 
-    numbers.push(numbers4Field.value)
+    // Verifico se i numeri combaciano
+    if (randomNumbers.includes(thisNumber)) {
+      checkNumbers.push(thisNumber);
+      checkCounter = checkCounter + 1;
+    }
+  }
 
-    numbers.push(numbers5Field.value)
+  // Mostro quanti numeri sono corretti
+  console.log(checkCounter);
 
-    console.log(numbers)
-})
+  // Mostro quali numeri sono corretti
+  console.log(checkNumbers);
+});
 
 /* Verifica correttezza numeri */
 
