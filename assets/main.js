263bc3581593ff62aba12b52788152a7fe5cf5f8
maksbernatskyy <maksbernatskyy@gmail.com>
2025-10-16 13:45:20 +0200

    push random numbers in html

--- assets/main.js
+++ assets/main.js
@@ -17,14 +17,22 @@ function getRandomNumber(min, max) {
 
 /* Inizializzazione variabili */
 
+// Collegamento al div nell'HTML dei numeri random
+const randomNumbersField = document.getElementById('random-numbers')
+
 // Contenitore dei numeri generati
 const randomNumbers = []
 
 /* Generazione randomica dei numeri */
+
+// Ciclo di inserimento numeri random
 for (let i = 0; i < 5; i++) {
     randomNumbers.push(getRandomNumber(1, 100))
 }
 console.log(randomNumbers)
+
+// Inserimento numeri nell'HTML
+randomNumbersField.innerHTML = randomNumbers
 
 /* Inserimento numeri dell'utente */
 
