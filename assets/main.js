2447d66adf5d96dc689741d93cd1586289e65bf3
maksbernatskyy <maksbernatskyy@gmail.com>
2025-10-16 13:34:59 +0200

    add funtion for generate random numbers

--- assets/main.js
+++ assets/main.js
@@ -10,9 +10,20 @@ Tools:
 - for
 */
 
+/* Inizializzazione funzioni */
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
 /* Inizializzazione variabili */
 
+// Contenitore dei numeri generati
+const randomNumbers = []
+
 /* Generazione randomica dei numeri */
+for (let i = 0; i < 5; i++) {
+    
+}
 
 /* Inserimento numeri dell'utente */
 
