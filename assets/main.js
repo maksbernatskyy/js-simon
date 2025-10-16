593e2cead410c7104cdc215f2a478c7ada01f657
maksbernatskyy <maksbernatskyy@gmail.com>
2025-10-16 13:27:43 +0200

    add tools and macrotasks

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,22 @@
-console.log('It Works')
+/*
+Tools:
+
+- function
+- random
+- const / let
+- setTime
+- if / else
+- array
+- for
+*/
+
+/* Inizializzazione variabili */
+
+/* Generazione randomica dei numeri */
+
+/* Inserimento numeri dell'utente */
+
+/* Verifica correttezza numeri */
+
+/* Riferisco quanti e quali numeri sono stati indovinati */ 
+
