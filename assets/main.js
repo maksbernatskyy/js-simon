9365cefb5c92da5a7ce46a8c13988fcd232ebdb5
maksbernatskyy <maksbernatskyy@gmail.com>
2025-10-16 13:46:20 +0200

    add link for Bootstrap

--- assets/main.js
+++ assets/main.js
@@ -12,31 +12,30 @@ Tools:
 
 /* Inizializzazione funzioni */
 function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 /* Inizializzazione variabili */
 
 // Collegamento al div nell'HTML dei numeri random
-const randomNumbersField = document.getElementById('random-numbers')
+const randomNumbersField = document.getElementById("random-numbers");
 
 // Contenitore dei numeri generati
-const randomNumbers = []
+const randomNumbers = [];
 
 /* Generazione randomica dei numeri */
 
 // Ciclo di inserimento numeri random
 for (let i = 0; i < 5; i++) {
-    randomNumbers.push(getRandomNumber(1, 100))
+  randomNumbers.push(getRandomNumber(1, 100));
 }
-console.log(randomNumbers)
+console.log(randomNumbers);
 
 // Inserimento numeri nell'HTML
-randomNumbersField.innerHTML = randomNumbers
+randomNumbersField.innerHTML = randomNumbers;
 
 /* Inserimento numeri dell'utente */
 
 /* Verifica correttezza numeri */
 
-/* Riferisco quanti e quali numeri sono stati indovinati */ 
-
+/* Riferisco quanti e quali numeri sono stati indovinati */
